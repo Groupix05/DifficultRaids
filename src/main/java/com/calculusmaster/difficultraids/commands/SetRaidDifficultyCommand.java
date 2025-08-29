bab671b390bcedb4e817a096dbd140899b98cb7c
Groupix05 <fox.bouche2@gmail.com>
2025-08-29 18:11:13 +0200

    Fixed SetRaidDifficultCommand

--- src/main/java/com/calculusmaster/difficultraids/commands/SetRaidDifficultyCommand.java
+++ src/main/java/com/calculusmaster/difficultraids/commands/SetRaidDifficultyCommand.java
@@ -16,88 +16,60 @@ public class SetRaidDifficultyCommand
 {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher)
     {
-        LiteralArgumentBuilder<CommandSourceStack> root = Commands.literal("difficultraids")
-                .then(Commands.literal("raiddifficulty")
-                        .then(Commands.literal("get").executes(css -> {
-                            ServerPlayer player = css.getSource().getPlayerOrException();
-                            Raid raid = player.serverLevel().getRaidAt(player.blockPosition());
+        LiteralArgumentBuilder<CommandSourceStack> raidDifficultyCommand = Commands.literal("raiddifficulty");
 
-                            if(raid == null)
-                            {
-                                if(!player.hasEffect(MobEffects.BAD_OMEN))
-                                    css.getSource().sendFailure(Component.literal("You must be in a Raid or have some level of Bad Omen to check the Raid Difficulty."));
-                                else
-                                {
-                                    int level = player.getEffect(MobEffects.BAD_OMEN).getAmplifier() + 1;
-                                    css.getSource().sendSuccess(
-                                            () -> Component.literal("Entering a Village will initiate a "
-                                                    + RaidDifficulty.get(level).getFormattedName()
-                                                    + " Raid with your current Bad Omen level."),
-                                            false
-                                    );
-                                }
-                            }
-                            else
-                            {
-                                css.getSource().sendSuccess(
-                                        () -> Component.literal("You're currently in a "
-                                                + RaidDifficulty.get(raid.getBadOmenLevel()).getFormattedName()
-                                                + " Raid."),
-                                        false
-                                );
-                            }
+        // raiddifficulty get
+        raidDifficultyCommand.then(Commands.literal("get").executes(css -> {
+            ServerPlayer player = css.getSource().getPlayerOrException();
+            Raid raid = player.serverLevel().getRaidAt(player.blockPosition());
 
-                            return 1;
-                        }))
-                        .then(Commands.literal("set").then(buildSetCommands()))
-                        .then(Commands.literal("info").executes(css -> {
-                            css.getSource().sendSuccess(
-                                    () -> Component.literal(
-                                            "To select a Raid Difficulty, obtain higher levels of Bad Omen. "
-                                                    + "Level 1 spawns a Default Vanilla Raid, and higher levels spawn tougher Raids "
-                                                    + "added by DifficultRaids (2: Hero, 3: Legend, 4: Master, 5: Grandmaster)."),
-                                    false
-                            );
-                            return 1;
-                        }))
-                );
+            if(raid == null)
+            {
+                if(!player.hasEffect(MobEffects.BAD_OMEN))
+                    css.getSource().sendFailure(Component.literal("You must be in a Raid or have some level of Bad Omen to check the Raid Difficulty."));
+                else
+                {
+                    int level = player.getEffect(MobEffects.BAD_OMEN).getAmplifier() + 1;
 
-        dispatcher.register(root);
-    }
+                    css.getSource().sendSuccess(() -> Component.literal("Entering a Village will initiate a " + RaidDifficulty.get(level).getFormattedName() + " Raid with your current Bad Omen level."), false);
+                }
+            }
+            else css.getSource().sendSuccess(() -> Component.literal("You're currently in a " + RaidDifficulty.get(raid.getBadOmenLevel()).getFormattedName() + " Raid."), false);
 
-    private static LiteralArgumentBuilder<CommandSourceStack> buildSetCommands()
-    {
-        LiteralArgumentBuilder<CommandSourceStack> set = Commands.literal("dummy"); // remplacé après
+            return 1;
+        }));
 
+        // raiddifficulty set <difficulty>
         for(RaidDifficulty difficulty : RaidDifficulty.values())
         {
-            set.then(Commands.literal(difficulty.toString().toLowerCase())
-                    .requires(css -> {
-                        try { return css.getPlayerOrException().hasPermissions(2); }
-                        catch (CommandSyntaxException e) { e.printStackTrace(); return false; }
-                    })
-                    .executes(css -> {
-                        int level = switch(difficulty) {
-                            case DEFAULT -> 1;
-                            case HERO -> 2;
-                            case LEGEND -> 3;
-                            case MASTER -> 4;
-                            case GRANDMASTER -> 5;
-                        };
+            raidDifficultyCommand.then(Commands.literal("set").then(Commands.literal(difficulty.toString().toLowerCase()).requires(css -> {
+                try { return css.getPlayerOrException().hasPermissions(2); }
+                catch(CommandSyntaxException e) { e.printStackTrace(); return false; }
+            }).executes(css -> {
+                int level = switch(difficulty) {
+                    case DEFAULT -> 1;
+                    case HERO -> 2;
+                    case LEGEND -> 3;
+                    case MASTER -> 4;
+                    case GRANDMASTER -> 5;
+                };
 
-                        css.getSource().getPlayerOrException().addEffect(
-                                new MobEffectInstance(MobEffects.BAD_OMEN, 100000, level - 1)
-                        );
+                css.getSource().getPlayerOrException().addEffect(new MobEffectInstance(MobEffects.BAD_OMEN, 100000, level - 1));
 
-                        css.getSource().sendSuccess(
-                                () -> Component.literal("Entering a Village will initiate a " + difficulty.getFormattedName() + " Raid."),
-                                false
-                        );
-                        return 1;
-                    })
-            );
+                css.getSource().sendSuccess(() -> Component.literal("Entering a Village will initiate a " + difficulty.getFormattedName() + " Raid."), false);
+                return 1;
+            })));
         }
 
-        return set;
+        // raiddifficulty info
+        raidDifficultyCommand.then(Commands.literal("info").executes(css -> {
+            css.getSource().sendSuccess(() -> Component.literal("To select a Raid Difficulty, obtain higher levels of Bad Omen. Level 1 spawns a Default Vanilla Raid, and higher levels spawn tougher Raids added by DifficultRaids (2: Hero, 3: Legend, 4: Master, 5: Grandmaster)."), false);
+            return 1;
+        }));
+
+        LiteralArgumentBuilder<CommandSourceStack> mainCommand = Commands.literal("difficultraids")
+                .then(raidDifficultyCommand);
+
+        dispatcher.register(mainCommand);
     }
 }
